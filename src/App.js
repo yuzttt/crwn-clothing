5581b33abe2d20cafe9a89e68c0735b8b25b7bc6
ytz <ytz123@qq.com>
2022-02-18 16:06:58 +0800

    update our menu-items to navigate withRouter HDC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
+import {Switch,Route } from 'react-router-dom';
 import HomePage from './homepage/homepage.component';
+
+const HatsPage = () =>(
+  <div>
+  <h1>HATS PAGE </h1>
+  </div>
+);
+
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div>
+    <Switch>
+       <Route exact path='/' component={ HomePage} />
+       <Route path='/shop/hats' component={HatsPage} />
+    </Switch>
     </div>
   );
 }
